e124860247b253484cb6642dca9ffa5de5a788d6
Pavel Shutov <shutov.ps@gmail.com>
2018-02-19 20:20:18 +0300

    create main branch

--- RemoteFork.Plugins.Thvp/Commands/ICommand.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace RemoteFork.Plugins.Commands {
-    public interface ICommand {
-        List<Item> GetItems(IPluginContext context, params string[] data);
-    }
-}
